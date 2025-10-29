7929d73707368dd4ef5902ad2b9c8a278237a43a
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-10-29 13:34:56 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,15 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://highway-delite-experiences.vercel.app",
+    "http://localhost:3000",
+    "http://localhost:5173"
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // MongoDB Connection
